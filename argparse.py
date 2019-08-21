629ce92cdd6d8750d6d41e8a53dbff6d8ae72025
fangzy0723 <fangzy0723@sina.com>
2019-08-21 14:57:20 +0800

    Create argparse.py

---
+++ argparse.py
@@ -0,0 +1,9 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.description="这是测试命令传参帮助信息"
+parser.add_argument("-a","--age",dest="my_age",help="这是年龄参数",required=True,type=int)
+parser.add_argument("-n","--name",dest="my_name",help="这是姓名参数",required=True,type=str)
+
+args = parser.parse_args()
+
+print("姓名：%s，年龄：%s" % (args.my_name,args.my_age))
